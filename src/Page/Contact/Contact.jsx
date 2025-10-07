7ca05abf9f3405ae3339482aafc1a586a22d4dce
Mehedi Hasan Sagor <mehedisagor2001@gmail.com>
2025-10-07 08:24:49 +0600

    fgff

--- src/Page/Contact/Contact.jsx
+++ src/Page/Contact/Contact.jsx
@@ -1,5 +1,4 @@
-
-import gpsIcon from "../Contact/GPS.png"; 
+import gpsIcon from "../Contact/GPS.png";
 import phoneIcon from "../Contact/phone-call.png";
 import emailIcon from "../Contact/gmail (2).png";
 import facebookIcon from "../Contact/Facebook (5).png";
@@ -30,7 +29,6 @@ const Contact = () => {
             </div>
           </div>
 
-        
           <div className="flex items-center space-x-4 p-4 bg-white shadow-md rounded-lg">
             <img className="w-10 h-10" src={phoneIcon} alt="Phone" />
             <div>
@@ -39,7 +37,6 @@ const Contact = () => {
             </div>
           </div>
 
-        
           <div className="flex items-center space-x-4 p-4 bg-white shadow-md rounded-lg">
             <img className="w-10 h-10" src={emailIcon} alt="Email" />
             <div>
@@ -48,21 +45,34 @@ const Contact = () => {
             </div>
           </div>
 
-         
           <div>
-            <h2 className="text-xl font-semibold text-gray-700">Connect with Us</h2>
+            <h2 className="text-xl font-semibold text-gray-700">
+              Connect with Us
+            </h2>
             <div className="flex space-x-4 mt-2 aaaaa mx-7">
               <a href="#">
                 <img className="w-10 h-10 mx-5 mt-2" src={gpsIcon} alt="GPS" />
               </a>
               <a href="#">
-                <img className="w-10 h-10 mx-5 mt-2" src={emailIcon} alt="Email" />
+                <img
+                  className="w-10 h-10 mx-5 mt-2"
+                  src={emailIcon}
+                  alt="Email"
+                />
               </a>
               <a href="#">
-                <img className="w-10 h-10 mx-5 mt-2" src={phoneIcon} alt="Phone" />
+                <img
+                  className="w-10 h-10 mx-5 mt-2"
+                  src={phoneIcon}
+                  alt="Phone"
+                />
               </a>
               <a href="#">
-                <img className="w-10 h-10 mx-5 mt-2" src={facebookIcon} alt="Facebook" />
+                <img
+                  className="w-10 h-10 mx-5 mt-2"
+                  src={facebookIcon}
+                  alt="Facebook"
+                />
               </a>
             </div>
           </div>
@@ -70,7 +80,9 @@ const Contact = () => {
 
         {/* Contact Form */}
         <div className="bg-white p-6 shadow-md rounded-lg">
-          <h2 className="text-3xl font-bold text-gray-800 text-center mb-4">Contact </h2>
+          <h2 className="text-3xl font-bold text-gray-800 text-center mb-4">
+            Contact{" "}
+          </h2>
           <form className="space-y-4">
             <input
               type="text"
